1fa4f92ae66b44de0e300a367edf0960da94a7fc
hsm207 <hsm207@users.noreply.github.com>
2021-05-27 10:35:35 +0000

    Add sample JA corpus for time dimension

---
+++ Duckling/Time/JA/Corpus.hs
@@ -0,0 +1,41 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree.
+
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Duckling.Time.JA.Corpus
+  ( corpus
+--   , defaultCorpus
+--   , negativeCorpus
+--   , latentCorpus
+--   , diffCorpus
+  ) where
+
+import Data.String
+import Prelude
+
+import Duckling.Core
+import Duckling.Testing.Types hiding (examples)
+import Duckling.Time.Corpus
+import Duckling.Time.Types hiding (Month, refTime)
+import Duckling.TimeGrain.Types hiding (add)
+
+corpus :: Corpus
+corpus = (testContext, testOptions, allExamples)
+
+allExamples :: [Example]
+allExamples = concat
+  [ examples (datetime (2013, 2, 12, 0, 0, 0) Day)
+             [ "today"
+             ]
+  , examples (datetime (2013, 2, 11, 0, 0, 0) Day)
+             [ "yesterday"
+             ]
+  , examples (datetime (2013, 2, 13, 0, 0, 0) Day)
+             [ "tomorrow"
+             ]
+  ]
